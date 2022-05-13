85776ccddf6ae5b1e07220904b5178d4e551fc53
jlf44 <jlf44@cam.ac.uk>
2022-05-13 23:14:19 +0100

    Adding s3 bucket versioning

--- modules/cudl-data-processing/s3.tf
+++ modules/cudl-data-processing/s3.tf
@@ -11,6 +11,27 @@ resource "aws_s3_bucket" "transcriptions-bucket" {
   bucket = lower("${var.environment}-${var.transcriptions-bucket-name}")
 }
 
+resource "aws_s3_bucket_versioning" "source-bucket-versioning" {
+  count = var.db-only-processing ? 0 : 1
+  bucket = aws_s3_bucket.source-bucket[0].id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+resource "aws_s3_bucket_versioning" "dest-bucket-versioning" {
+  bucket = aws_s3_bucket.dest-bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "transcriptions-bucket-versioning" {
+  bucket = aws_s3_bucket.transcriptions-bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
 resource "aws_s3_bucket_notification" "source-bucket-notifications" {
 
   #count  = length(var.transform-lambda-information)
