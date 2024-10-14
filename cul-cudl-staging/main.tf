9e8c83616a5f4ecdfd49f1f94b751b2833261dbe
Rory McHugh <rmm98@cam.ac.uk>
2024-10-14 11:53:48 +0100

    feat(cul-cudl-staging): Update default WAF to remove IP set
    - Update module version to v1.7.2 in base_architecture module
    - Remove cudl_viewer_waf.tf and content_loader_waf.tf
    - Update workload modules to use module.base_architecture.waf_acl_arn
    - Update terraform version to 1.9.7

--- cul-cudl-staging/main.tf
+++ cul-cudl-staging/main.tf
@@ -1,5 +1,5 @@
 module "base_architecture" {
-  source = "git::https://github.com/cambridge-collection/terraform-aws-architecture-ecs.git?ref=v1.7.0"
+  source = "git::https://github.com/cambridge-collection/terraform-aws-architecture-ecs.git?ref=v1.7.2"
 
   name_prefix                    = local.base_name_prefix
   ec2_instance_type              = var.ec2_instance_type
@@ -89,7 +89,7 @@ module "content_loader" {
   asg_security_group_id      = module.base_architecture.asg_security_group_id
   alb_security_group_id      = module.base_architecture.alb_security_group_id
   cloudwatch_log_group_arn   = module.base_architecture.cloudwatch_log_group_arn
-  cloudfront_waf_acl_arn     = aws_wafv2_web_acl.content_loader.arn
+  cloudfront_waf_acl_arn     = module.base_architecture.waf_acl_arn
   cloudfront_allowed_methods = var.content_loader_allowed_methods
   iam_task_additional_policies = {
     staging_releases    = aws_iam_policy.staging_cudl_data_releases.arn,
@@ -232,7 +232,7 @@ module "cudl_viewer" {
   asg_security_group_id                  = module.base_architecture.asg_security_group_id
   alb_security_group_id                  = module.base_architecture.alb_security_group_id
   cloudwatch_log_group_arn               = module.base_architecture.cloudwatch_log_group_arn
-  cloudfront_waf_acl_arn                 = aws_wafv2_web_acl.cudl_viewer.arn # custom WAF ACL for CUDL Viewer
+  cloudfront_waf_acl_arn                 = module.base_architecture.waf_acl_arn
   cloudfront_allowed_methods             = var.cudl_viewer_allowed_methods
   cloudfront_viewer_request_function_arn = aws_cloudfront_function.viewer.arn
   efs_use_existing_filesystem            = true
