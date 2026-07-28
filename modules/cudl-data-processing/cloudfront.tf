92c373013725e3e65fa63c0438c8b45fb3f18b4b
Michael Hawkins <mjh39@cam.ac.uk>
2026-07-28 10:09:47 +0100

    feat(cudl-data-processing): configurable CloudFront cache policies and ordered behaviors

--- modules/cudl-data-processing/cloudfront.tf
+++ modules/cudl-data-processing/cloudfront.tf
@@ -1,6 +1,15 @@
-data "aws_cloudfront_cache_policy" "managed_caching_disabled" {
+locals {
+  occb = var.cloudfront_ordered_cache_behaviors
+  cache_policy_names = toset(concat(
+    [var.cloudfront_default_cache_policy],
+    [for b in local.occb : b.cache_policy_name],
+  ))
+}
+
+data "aws_cloudfront_cache_policy" "selected" {
+  for_each = local.cache_policy_names
   provider = aws.us-east-1
-  name     = "Managed-CachingDisabled"
+  name     = each.value
 }
 
 resource "aws_cloudfront_origin_access_control" "this" {
@@ -43,13 +52,34 @@ resource "aws_cloudfront_distribution" "this" {
     smooth_streaming       = false
     target_origin_id       = local.cloudfront_distribution_domain_name
     viewer_protocol_policy = "redirect-to-https"
-    cache_policy_id        = data.aws_cloudfront_cache_policy.managed_caching_disabled.id
+    cache_policy_id        = data.aws_cloudfront_cache_policy.selected[var.cloudfront_default_cache_policy].id
 
     dynamic "function_association" {
       for_each = var.cloudfront_viewer_request_function_arn != null ? [1] : []
       content {
         event_type   = "viewer-request"
         function_arn = var.cloudfront_viewer_request_function_arn
+      }
+    }
+  }
+
+  dynamic "ordered_cache_behavior" {
+    for_each = local.occb
+    content {
+      path_pattern           = ordered_cache_behavior.value.path_pattern
+      allowed_methods        = ordered_cache_behavior.value.allowed_methods
+      cached_methods         = ordered_cache_behavior.value.cached_methods
+      compress               = ordered_cache_behavior.value.compress
+      target_origin_id       = local.cloudfront_distribution_domain_name
+      viewer_protocol_policy = "redirect-to-https"
+      cache_policy_id        = data.aws_cloudfront_cache_policy.selected[ordered_cache_behavior.value.cache_policy_name].id
+
+      dynamic "function_association" {
+        for_each = ordered_cache_behavior.value.attach_viewer_request_function && var.cloudfront_viewer_request_function_arn != null ? [1] : []
+        content {
+          event_type   = "viewer-request"
+          function_arn = var.cloudfront_viewer_request_function_arn
+        }
       }
     }
   }
